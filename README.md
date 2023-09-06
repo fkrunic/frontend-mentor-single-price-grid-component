00d7fc4e2a632ad897683e8d5a3381d3440563fe
Filip Krunic <fkrunic9@gmail.com>
2023-09-06 13:51:57 -0700

    project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Single Price Grid Component
+Implementation of [Single Price Grid Component](https://www.frontendmentor.io/challenges/single-price-grid-component-5ce41129d0ff452fec5abbbc/hub) using Vue and TailwindCSS.
---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <p class="text-3xl font-bold underline">Hello world!</p>
+  </div>
+</template>
