8908661109fb736a70fb1b8aea33817579d728c9
Filip Krunic <fkrunic9@gmail.com>
2023-09-06 22:23:32 -0700

    fixed base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'frontend-mentor-single-price-grid-component/',
+  base: '/frontend-mentor-single-price-grid-component/',
   plugins: [vue()],
 })
