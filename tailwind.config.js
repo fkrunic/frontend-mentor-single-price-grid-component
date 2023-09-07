f7fae008c067fc50fdda31ed4da91fefc671a395
Filip Krunic <fkrunic9@gmail.com>
2023-09-06 22:05:04 -0700

    mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
+    screen: {
+      desktop: '1440px'
+    },
     fontFamily: {
       'karla': 'Karla, sans-serif'
+    },
+    colors: {
+      'cyan': 'hsl(179, 62%, 43%)',
+      'bright-yellow': 'hsl(71, 73%, 54%)',
+      'light-gray': 'hsl(204, 43%, 93%)',
+      'grayish-blue': 'hsl(218, 22%, 67%)',
+      'white': 'hsl(0, 0%, 100%)'
     },
     extend: {},
   },
